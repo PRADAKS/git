55b8dc26cd2047b4c8b230a2b119db50f8bedf84
PRADAKS <pradakshinas583223104078@nprcolleges.org>
2024-11-04 16:12:35 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,10 @@
+def mymax(list1):
+    print("Largest element is:",max(list1))
+    list1=[]
+    num=int(input("Enter number of element in list:"))
+    for i in range (1,num+1):
+        ele=int(input("Enter elements:"))
+        list1.append(ele)
+        print("Largest element is:,"mymax(list 1))
+        
+          
